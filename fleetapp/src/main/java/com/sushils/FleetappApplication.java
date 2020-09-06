d5810911989a24f1e3d74112eedfb370ad4a0d02
sushils <sushilsangle.ss@gmail.com>
2020-09-06 23:38:59 +0530

    FleetappApplication class updated with new annotations.

--- fleetapp/src/main/java/com/sushils/FleetappApplication.java
+++ fleetapp/src/main/java/com/sushils/FleetappApplication.java
@@ -2,8 +2,10 @@ package com.sushils;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+
 public class FleetappApplication {
 
 	public static void main(String[] args) {
