4c81bba223a6f7deb139bdf78ea58aa368b2e896
sushils <sushilsangle.ss@gmail.com>
2020-09-06 23:52:11 +0530

    VehicleMovement model updated .

--- fleetapp/src/main/java/com/sushils/model/VehicleMovement.java
+++ fleetapp/src/main/java/com/sushils/model/VehicleMovement.java
@@ -1,47 +1,57 @@
 package com.sushils.model;
 
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "id")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class VehicleMovement {
 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name="id")
     private int id;
 
     @ManyToOne
-    @JoinColumn(name = "vehicleid",insertable = false, updatable = false)
+    @JoinColumn(name="vehicleid", insertable=false, updatable=false)
     private Vehicle vehicle;
-    private Vehicle vehicleid;
+    private int vehicleid;
 
     @ManyToOne
-    @JoinColumn(name = "locationid1",insertable = false,updatable = false)
+    @JoinColumn(name="locationid1", insertable=false, updatable=false)
     private Location location1;
-    private Location locationid1;
+    private int locationid1;
 
-    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date1;
 
     @ManyToOne
-    @JoinColumn(name = "locationid2", insertable = false,updatable = false)
+    @JoinColumn(name="locationid2", insertable=false, updatable=false)
     private Location location2;
-    private Location locationid2;
+    private int locationid2;
 
-    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date2;
 
     private String remarks;
