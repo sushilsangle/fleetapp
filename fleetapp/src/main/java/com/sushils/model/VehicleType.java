100dc97155fa381d74eeebaaeb60853225bc14ae
sushils <sushilsangle.ss@gmail.com>
2020-09-06 23:53:00 +0530

    VehicleType model updated.

--- fleetapp/src/main/java/com/sushils/model/VehicleType.java
+++ fleetapp/src/main/java/com/sushils/model/VehicleType.java
@@ -7,13 +7,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "id")
 @EqualsAndHashCode(callSuper = false)
 public class VehicleType extends CommonObject {
