aa2f8ad9a067cb374e7b4f1f6bc52d18780febae
sushils <sushilsangle.ss@gmail.com>
2020-09-06 23:35:31 +0530

    ApplicationController updated.

--- fleetapp/src/main/java/com/sushils/ApplicationController.java
+++ fleetapp/src/main/java/com/sushils/ApplicationController.java
@@ -2,8 +2,9 @@ package com.sushils;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ApplicationController {
     @GetMapping("/index")
     public String goHome()
