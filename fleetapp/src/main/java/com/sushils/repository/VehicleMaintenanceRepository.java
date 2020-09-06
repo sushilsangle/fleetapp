6b79a1c44ff846d62a20838210305c18bf4e76b9
sushils <sushilsangle.ss@gmail.com>
2020-09-06 23:54:01 +0530

    VehicleMaintenanceRepository updated.

--- fleetapp/src/main/java/com/sushils/repository/VehicleMaintenanceRepository.java
+++ fleetapp/src/main/java/com/sushils/repository/VehicleMaintenanceRepository.java
@@ -1,8 +1,9 @@
 package com.sushils.repository;
 
+import com.sushils.model.VehicleMaintenance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VehicleMaintenanceRepository extends JpaRepository<VehicleMaintenanceRepository,Integer> {
+public interface VehicleMaintenanceRepository extends JpaRepository<VehicleMaintenance,Integer> {
 }
